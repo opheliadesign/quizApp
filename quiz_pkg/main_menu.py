55af40ee8c1ea84194c6a2062e8cdf0586de1e57
Benjamin McMahan <benjamin@linkedresponder.com>
2022-05-25 12:13:10 -0400

    Updated main menu's styling, added initial options

--- quiz_pkg/main_menu.py
+++ quiz_pkg/main_menu.py
@@ -1,10 +1,20 @@
+"""
+quizApp Main Menu
+
+"""
 from rich import print
+from rich import box
+from rich.align import Align
+from rich.console import Console, Group
 from rich.layout import Layout
 from rich.panel import Panel
+from rich.table import Table
+
+console = Console()
 
 
 def make_layout() -> Layout:
-    """Define the layout"""
+    """Define the layout."""
     layout = Layout(name="root")
 
     layout.split(
@@ -18,12 +28,67 @@ def make_layout() -> Layout:
     return layout
 
 
+def make_menu_options() -> Panel:
+    """Some example content."""
+    menu_options = Table.grid(padding=1)
+    menu_options.add_column(style="green", justify="left")
+    menu_options.add_column(no_wrap=True)
+    menu_options.add_row(
+        "1)",
+        "Start a Quiz",
+    )
+    menu_options.add_row(
+        "2)", "Review Last Quiz",
+    )
+    menu_options.add_row(
+        "3)", "Select Category",
+    )
+    menu_options.add_row(
+        "4)", "Logout"
+    )
+
+    message = Table.grid(padding=1)
+    message.add_column()
+    message.add_column(no_wrap=True)
+
+    message_panel = Panel(
+        Align.center(
+            Group(Align.center(menu_options)),
+            vertical="middle",
+        ),
+        box=box.ROUNDED,
+        padding=(1, 2),
+        title="[b cyan]Main Menu",
+        border_style="bright_blue",
+    )
+    return message_panel
+
+
 class Header:
-    # Maybe use a table here?
     def __rich__(self) -> Panel:
-        return Panel("Choose an option from the menu below!", title="Welcome", style="white on blue")
+        grid = Table.grid(expand=True)
+        grid.add_column(justify="center", ratio=1)
+        grid.add_row(
+            "[b]quizApp[/b] Let's review!",
+        )
+        return Panel(grid, style="white on blue")
 
 
-class Body:
+class Side:
     def __rich__(self) -> Panel:
-        return Panel("Choose from the options below", title="Main Menu")
+        side_content = "Review recent incorrect answers"
+        # TODO Make this into a table
+        return Panel(Align.center(
+            Group(Align.center(side_content))
+        ),
+            title="Review",
+            border_style="red"
+        )
+
+
+def display_main_menu():
+    layout = make_layout()
+    layout["header"].update(Header())
+    layout["body"].update(make_menu_options())
+    layout["side"].update(Side())
+    print(layout)
